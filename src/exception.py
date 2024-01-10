18a6f7e604d2d3ae30e51555e525fa451022a900
Daniel Rawlins <danielpr211@hotmail.com>
2024-01-10 03:57:46 -0500

    Exception complete

--- src/exception.py
+++ src/exception.py
@@ -1,12 +1,12 @@
 import os, sys
-from src.logger import logging
+from logger import logging
 
 
 
 def error_message_detailed(error, error_detailed:sys):
     _, _, exc_tb = error_detailed.exc_info()
 
-    file_name = exc_tb.tb_frame.f_code.cp_filename 
+    file_name = exc_tb.tb_frame.f_code.co_filename 
 
     error_message = "Error occured in python script nam [{0}] line number [{1}] error message [{2}]".format(
         file_name,exc_tb.tb_lineno, str(error)
@@ -19,12 +19,13 @@ class CustomException(Exception):
         super().__init__(error_message)
         self.error_message = error_message_detailed(error_message, error_detailed=error_detailed)
 
-    def __str__(str):
+    def __str__(self):
         return self.error_message
 
 if __name__ == "__main__":
     try:
         a = 1/0
+    
     except Exception as e:
         logging.info("Division by zero")
         raise CustomException(e,sys)
